500d6e56b741d493b66acd123022a2506009b230
Valeria <valeriakirkbride@gmail.com>
2023-10-02 16:04:20 -0600

    taking break cors issue

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let addToy = false;
+const url = "http://localhost:3000/toys";
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -12,4 +13,74 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  fetchData();
+  submitToy();
 });
+
+function fetchData() {
+  fetch(url)
+  .then(res => res.json())
+  .then(toys => cardDivs(toys))
+  .catch(error => "Error: " + error)
+}
+
+function postData(url, data = {}) {
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+    body: JSON.stringify(data), 
+  });
+}
+
+
+function cardDivs(toys) {
+  const toyCollection = document.getElementById("toy-collection");
+  toys.forEach(toy => {
+    let div = document.createElement("div");
+    div.classList.add("card");
+    toyCollection.appendChild(div)
+
+    let name = document.createElement("h2");
+    name.innerText = toy.name;
+    div.appendChild(name);
+
+    let image = document.createElement("img");
+    image.src = toy.image;
+    image.classList.add("toy-avatar");
+    div.appendChild(image);
+
+    let likes = document.createElement("p");
+    likes.innerText = `${toy.likes} Likes`;
+    div.appendChild(likes);
+    
+    let button = document.createElement("button");
+    button.classList.add("button");
+    button.textContent = "Likes";
+    div.appendChild(button);
+
+    button.addEventListener("click", () => {
+      likes.innerText = `${parseInt(likes.innerText) + 1} Likes`;
+    });
+  });
+}
+
+function submitToy() {
+  const form = document.querySelector(".add-toy-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = document.querySelectorAll(".input-text");
+    const data = {};
+    Array.from(formData).forEach((input) => {
+      data[input.name] = input.value;
+    });
+
+    postData(data);
+  })
+}
