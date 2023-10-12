190dd6abb2becd96b536ddbf409119903506d7e8
Valeria <valeriakirkbride@gmail.com>
2023-10-12 16:08:17 -0600

    toy tale

--- src/index.js
+++ src/index.js
@@ -17,34 +17,19 @@ document.addEventListener("DOMContentLoaded", () => {
   submitToy();
 });
 
-function fetchData() {
+function fetchData(data) {
   fetch(url)
   .then(res => res.json())
   .then(toys => cardDivs(toys))
   .catch(error => "Error: " + error)
-}
-
-function postData(url, data = {}) {
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type",
-    },
-    body: JSON.stringify(data), 
-  });
-}
-
+};
 
 function cardDivs(toys) {
   const toyCollection = document.getElementById("toy-collection");
   toys.forEach(toy => {
     let div = document.createElement("div");
     div.classList.add("card");
-    toyCollection.appendChild(div)
+    toyCollection.appendChild(div);
 
     let name = document.createElement("h2");
     name.innerText = toy.name;
@@ -56,20 +41,37 @@ function cardDivs(toys) {
     div.appendChild(image);
 
     let likes = document.createElement("p");
-    likes.innerText = `${toy.likes} Likes`;
+    likes.innerText = `${toy.likes || 0} Likes`;
     div.appendChild(likes);
-    
+
     let button = document.createElement("button");
     button.classList.add("button");
     button.textContent = "Likes";
     div.appendChild(button);
 
     button.addEventListener("click", () => {
-      likes.innerText = `${parseInt(likes.innerText) + 1} Likes`;
+      let newNumberOfLikes = parseInt(likes.innerText) + 1;
+      likes.innerText = `${newNumberOfLikes} Likes`;
+      toy.likes = newNumberOfLikes;
+    
+      fetch(url + `/${toy.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({ likes: newNumberOfLikes }),
+      })
+        .then(res => res.json())
+        .then(updatedToy => {
+          console.log(updatedToy);
+        })
+        .catch(error => {
+          console.error("Error:", error);
+        });
     });
-  });
-}
-
+  }
+)};
 function submitToy() {
   const form = document.querySelector(".add-toy-form");
   form.addEventListener("submit", (event) => {
@@ -80,7 +82,20 @@ function submitToy() {
     Array.from(formData).forEach((input) => {
       data[input.name] = input.value;
     });
-
-    postData(data);
-  })
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then(res => res.json())
+      .then(newToy => {
+        cardDivs([newToy]);
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+  });
 }
